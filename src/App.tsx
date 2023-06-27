5303bdf1b7077adeb7bc6f0b22f3d27a9acd51d7
yuta-nishi <ypmtk437.0209@gmail.com>
2023-06-27 23:00:19 +0900

    feat: update demo view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,78 @@
 import { MiniReact } from './mini-react';
+import { useEffect, useState } from './mini-react/hooks';
+
+interface PostData {
+  isLoading: boolean;
+  user: {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  };
+}
+
+const initialPostData: PostData = {
+  isLoading: false,
+  user: {
+    userId: 0,
+    id: 0,
+    title: '',
+    body: '',
+  },
+};
 
 export const App = () => {
-  return <p>Hello World!</p>;
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState<PostData>(initialPostData);
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+      const resData = await response.json();
+
+      setData(() => {
+        return {
+          isLoading: true,
+          user: resData,
+        };
+      });
+    };
+
+    const timer = setTimeout(fetchPostData, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const countUpHandler = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const countDownHandler = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1 style={{ color: 'green' }}>Count: {count}</h1>
+      <button type="button" onClick={countUpHandler}>
+        count up
+      </button>
+      <button type="button" onClick={countDownHandler}>
+        count down
+      </button>
+      <hr />
+      {data.isLoading ? (
+        <div>
+          <h1 style={{ color: 'blue' }}>user information</h1>
+          <p>id: {data.user.id}</p>
+          <p>title: {data.user.title}</p>
+          <p>body: {data.user.body}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
--- src/mini-react/global.d.ts
+++ src/mini-react/global.d.ts
@@ -1,5 +1,6 @@
 declare namespace JSX {
   interface IntrinsicElements {
+    h1: HTMLAttributes<HTMLHeadingElement>;
     div: HTMLAttributes<HTMLDivElement>;
     p: HTMLAttributes<HTMLParagraphElement>;
     button: HTMLAttributes<HTMLButtonElement>;
